2adc57b014d1df47e160dc9875ff229b8c940db9
Eric Brengle <eric.brengle@gmail.com>
2015-02-06 16:14:38 -0600

    fix

--- karma.conf.js
+++ karma.conf.js
@@ -27,11 +27,16 @@ module.exports = function(config) {
     exclude: [
     ],
 
+ // test results reporter to use
+    // possible values: 'dots', 'progress'
+    // available reporters: https://npmjs.org/browse/keyword/karma-reporter
+    reporters: ['progress', 'coverage'],
+
 
     // preprocess matching files before serving them to the browser
     // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
     preprocessors: {
-      'public/js/**/*.html': ['coverage']
+      'public/js/**/*.js': ['coverage']
     },
 
     coverageReporter: {
@@ -40,11 +45,7 @@ module.exports = function(config) {
     },
 
 
-    // test results reporter to use
-    // possible values: 'dots', 'progress'
-    // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-    reporters: ['progress', 'coverage'],
-
+ 
 
     // web server port
     port: 9876,
